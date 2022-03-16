fb0f4018d1582ba599df61e416b25016c753f136
Semandr <semandr.code@gmail.com>
2022-03-16 15:02:20 +0100

    Write the roll dice functionality. Part 2. Switching the active player.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ const diceEl = document.querySelector('.dice');
 const bntNew = document.querySelector('.btn__new');
 const btnRoll = document.querySelector('.btn__roll');
 const btnHold = document.querySelector('.btn__hold');
+const player0 = document.querySelector('.player__0');
+const player1 = document.querySelector('.player__1');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0El.textContent = 0;
@@ -16,7 +20,7 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 // Rolling dice functionality
-btnRoll.addEventListener('click', e => {
+btnRoll.addEventListener('click', () => {
   // Generating a random dice roll
   const diceNum = Math.trunc(Math.random() * 6) + 1;
   // Display dice
@@ -26,7 +30,16 @@ btnRoll.addEventListener('click', e => {
   if (diceNum !== 1) {
     // Add the dice to the current csore
     currentScore += diceNum;
-    current0El.textContent = currentScore;
+    document.getElementById(`current-${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // Resetting the current score
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    // Switch the player
+    // Here we are reasign an active player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player__active');
+    player1.classList.toggle('player__active');
   }
 });
